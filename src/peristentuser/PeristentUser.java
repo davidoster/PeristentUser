f4c860b8a0ac59e9d407eb0845bd2659933155f2
David Oster <paspa@hotmail.com>
2020-04-28 11:02:07 +0300

    Latest Code

---
+++ src/peristentuser/PeristentUser.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peristentuser;
+
+import entities.User;
+
+/**
+ *
+ * @author mac
+ */
+public class PeristentUser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+            findById(int id)
+            findAll()
+            save(User user) // insert into
+            update(int id, User user)
+            delete(int id)
+        
+        */
+        services.UserImpl userService = new services.UserImpl();
+        User user = userService.findById(1);
+        if(user == null) System.out.println("Oh!!! Something is not found!!");
+    }
+    
+}
---
+++ src/services/UserImpl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entities.User;
+
+/**
+ *
+ * @author mac
+ */
+public class UserImpl implements IUser {
+
+    @Override
+    public User findById(long id) {
+        if(id <= 0) 
+            return null;
+        else {
+            // DAO = Data Access Object
+            dao.UserDaoImpl userDao = new dao.UserDaoImpl();
+            return userDao.findById(id);
+        } 
+    }
+    
+}
